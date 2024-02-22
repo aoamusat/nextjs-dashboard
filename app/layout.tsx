a72f752c418950309f5851bec885420d46af2146
Akeem Amusat <aoamusat@gmail.com>
2024-02-22 09:10:29 +0100

    Web worker

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
+"use client"
 import "@/app/ui/global.css"
 import { inter } from '@/app/ui/fonts';
+import { useEffect } from "react";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => console.log('scope is: ', registration.scope));
+    }
+  }, []);
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
